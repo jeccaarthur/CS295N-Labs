cc9b6da850177b6ec5f6de53d9916aa04bcba219
Jecca Arthur <arthurj@my.lanecc.edu>
2020-11-01 09:24:38 -0800

    Added quiz pages and related unit testing

--- JeccaArthurSite/JeccaArthurSite/Controllers/InfoController.cs
+++ JeccaArthurSite/JeccaArthurSite/Controllers/InfoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Winterfell.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,6 +30,19 @@ namespace Winterfell.Controllers
         public IActionResult History()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Quiz()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Quiz(QuizVM quiz)
+        {
+            quiz.CheckAnswers();
+            return View(quiz);
         }
     }
 }
